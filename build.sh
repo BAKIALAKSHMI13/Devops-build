1070435ea606a5c6d52c773b2711e314ac44be17
BAKIALAKSHMI13 <94897213+BAKIALAKSHMI13@users.noreply.github.com>
2025-10-22 17:24:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,29 +2,29 @@
 set -e
 
 # Usage: ./build.sh <branch_name>
-BRANCH=$GIT_BRANCH # Default to 'dev' if not provided
 
 IMAGE_NAME="react-static-app"
-DEV_IMAGE="bakialakshmi/dev:$BRANCH"
-PROD_IMAGE="bakialakshmi/prod:$BRANCH"
+DEV_IMAGE="bakialakshmi/dev:$BRANCH_NAME"
+PROD_IMAGE="bakialakshmi/prod:$BRANCH_NAME"
 
 echo "Building Docker image..."
 docker build -t $IMAGE_NAME:latest .
 
 # Docker Login (non-interactive mode for Jenkins)
 echo "Logging in to Docker Hub..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-if [ "$BRANCH" == "dev" ]; then
+if [ "$BRANCH_NAME" == "dev" ]; then
     echo "Tagging and pushing image to dev repo..."
     docker tag $IMAGE_NAME:latest $DEV_IMAGE
+    docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
     docker push $DEV_IMAGE
-elif [ "$BRANCH" == "master" ]; then
+elif [ "$BRANCH_NAME" == "master" ]; then
     echo "Tagging and pushing image to prod repo..."
     docker tag $IMAGE_NAME:latest $PROD_IMAGE
+    docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
     docker push $PROD_IMAGE
 else
-    echo "Unknown branch '$BRANCH'. Tagging as latest locally."
+    echo "Unknown branch '$BRANCH_NAME'. Tagging as latest locally."
 fi
 
-echo "Build and push completed for branch: $BRANCH"
+echo "Build and push completed for branch: $BRANCH_NAME"
